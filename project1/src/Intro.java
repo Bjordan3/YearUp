d4a65479b98c963dc19d669f8fe6fb050df9fb5d
Brandon <bjordan@atl.yearup.org>
2015-09-22 14:44:18 -0400

    In there

---
+++ project1/src/Intro.java
@@ -0,0 +1,9 @@
+
+public class Intro {
+	public static void main(String[] args) {
+	System.out.println("My name is Brandon");
+	System.out.println("My favorite movie is Star War");
+	System.out.println("I like to play League of Legends.");
+	}
+
+}
